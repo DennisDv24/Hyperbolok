5a30bc0d2686cdd61cb759d032e72cfe3bfbea95
DennisDv24 <jokarboxing@gmail.com>
2021-07-20 16:58:07 +0200

    Minor refactor

--- DialoguetriggerController.cs
+++ DialoguetriggerController.cs
@@ -8,14 +8,14 @@ public class DialoguetriggerController : MonoBehaviour
 	public GameObject dialogueToAdd;
 	public GameObject dialogueCanvas;
 	
-	private void OnTriggerEnter(Collider col)
+	private void OnTriggerEnter(Collider playerCol)
     {
-		if (col.tag.Equals("Player")){
+		if (playerCol.tag.Equals("Player")){
 			var thisDialogue = Instantiate(dialogueToAdd, new Vector3(0, 0, 0), Quaternion.identity);
 			
 			thisDialogue.transform.parent = dialogueCanvas.transform;
 			thisDialogue.transform.localPosition = new Vector3(0,0,0);
-			col.gameObject.GetComponent<PlayerController2>().movementIsBlocked = false;
+			playerCol.gameObject.GetComponent<PlayerController2>().movementIsBlocked = false;
 				
 		}
 	}
